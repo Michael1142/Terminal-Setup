8604d9380c80982e595dc7208933fe95459493d7
Michael1142 <michael1142@hotmail.co.uk>
2022-06-29 23:02:33 +0100

    add statement to clean output

--- terminal-Setup.sh
+++ terminal-Setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 brew_install() {
-    if brew list $1; then
+    if brew list $1 > /dev/null; then
         echo "${1} is already installed"
     else
         brew install $1 && echo "$1 is installed"
